15ab73b7e4a887726dccce7e048de0b2e91af917
Adnurhytia <adindanurhayati30@gmail.com>
2023-07-27 18:37:22 +0700

    project UAS pdw

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"></link>
+      <header className="App-header">
+        <img src="https://i.pinimg.com/474x/cb/99/66/cb9966a372e571380673ca88982453e2.jpg"></img>
+        <h1 class="text-center mt-3">B T S</h1>
+        <p class="col-md-8 fs-4">
+          BTS (Korean: 방탄소년단; RR: Bangtan Sonyeondan; lit. Bulletproof Boy Scouts),
+          also known as the Bangtan Boys, is a South Korean boy band formed in 2010.
+          The band consists of Jin, Suga, J-Hope, RM, Jimin, V, and Jungkook, who co-write or co-produce much of their material.
+        </p>
+        <a
+          className="App-link"
+          href="https://ibighit.com/bts/eng/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click Here
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
